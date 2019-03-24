97fa341e9c7a960bdc9ff70d4b9ac247245490ca
Owen Williams <owilliams@mixxx.org>
2019-03-24 18:11:11 -0400

    Add a prebuilt color wheel! Too bad red isn't at the top

--- templates/lights.js
+++ templates/lights.js
@@ -1,26 +1,3 @@
-// function ajax_submit(msg_selector, form_name) {
-//   var valuesToSubmit = $('form[name=' + form_name + ']').serialize();
-//   $.ajax({
-//     type: "PUT",
-//     url: "/votes", //submits it to the given url of the form
-//     data: valuesToSubmit,
-//     dataType: "JSON", // you want a difference between normal and ajax-calls, and json is standard
-//   }).done(function(){
-//     msg = "saved";
-//     saved_messages_shown++;
-
-//     if (saved_messages_shown >= wacky_message_threshold) {
-//       msg = success_messages[Math.floor(Math.random() * success_messages.length)];
-//     }
-
-//     flash_element(msg_selector, msg);
-//   }).fail(function(){
-//     flash_element(msg_selector, "failed");
-//   });
-
-//   return false; // prevents normal behaviour
-// }
-
 function load_light_names() {
   $.ajax({
     type: 'GET',
@@ -46,23 +23,54 @@ function load_presets() {
         $('#preset-list')
           .append(
               '<button type="button" class="btn btn-primary" name="button"' +
-              'onclick="activate_preset(&quot;' + presets[i] + '&quot;)">' + 
+              'onclick="activate_preset(&quot;' + presets[i] + '&quot;)">' +
               presets[i] + '</button>');
       }
     }
   })
 }
 
-function load_values() {
-  load_light_names();
-  load_presets();
+function onColorInputStart(color) {
+  mode = document.getElementById('modeSelect').value
+
+  // If the selected color has any s, then set mode to color.
+  hsl = color.hsl
+  if (mode === 'white' && hsl.s !== 0) {
+    // Change mode to color
+    $('#modeSelect').val('color');
+  }
+}
+
+function onColorChange(color, changes) {
+  // print the color's new hex value to the developer console
+  bulb = document.getElementById('lightSelect').value
+  if (bulb === '') {
+    return;
+  }
+
+  mode = document.getElementById('modeSelect').value
+
+  if (mode === 'color') {
+    color = color.hexString.substring(1);
+    $.ajax({
+      type: 'GET',
+      url: '/set_lights?bulb=' + bulb + '&rgbw=' + color + '00',
+    });
+  } else if (mode === 'white') {
+    color = color.hsl.l;
+    hex = Math.round(((color) / 100.0) * 255).toString(16);
+    $.ajax({
+      type: 'GET',
+      url: '/set_lights?bulb=' + bulb + '&rgbw=000000' + hex,
+    });
+  }
 }
 
 function activate_preset(name) {
   $.ajax({
     type: 'GET',
     url: '/activate_preset?name=' + name,
-  })
+  });
 }
 
 function set_power(onoff) {
@@ -73,5 +81,26 @@ function set_power(onoff) {
   $.ajax({
     type: 'GET',
     url: '/set_lights?bulb=' + bulb + '&power=' + onoff,
-  })
+  });
+}
+
+function load_values() {
+  load_light_names();
+  load_presets();
+
+  var colorPicker = new iro.ColorPicker('#color-picker-container', {
+    wheelLightness: false,
+  });
+  colorPicker.on('color:change', onColorChange);
+  colorPicker.on('input:start', onColorInputStart);
+  $('#modeSelect').change(function(ob) {
+    mode = document.getElementById('modeSelect').value
+    if (mode === "white") {
+      // Reset the colorpicker to 0% sat for whites.
+      curcolor = colorPicker.color.hsl;
+      curcolor.s = 0;
+      colorPicker.color.hsl = curcolor;
+    }
+    return true;
+  });
 }
