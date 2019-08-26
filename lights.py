992abd4e4ba1b57b0ea35378089df776ea6bd6a2
Owen Williams <owilliams@mixxx.org>
2019-08-26 12:09:22 -0400

    Flip the argument order because I often have to change the port

--- lights.py
+++ lights.py
@@ -8,15 +8,16 @@ import sys
 
 if __name__ == '__main__':
   if len(sys.argv) < 2:
-    print("Usage: lights.py [port] (--fake)")
+    print("Usage: lights.py (--fake) [port]")
     print(" --fake: Use fake lights, not real ones")
     sys.exit(1)
 
-  port = int(sys.argv[1])
   fake = False
   if len(sys.argv) > 2:
-    if sys.argv[2] == '--fake':
+    if sys.argv[1] == '--fake':
       fake = True
+
+  port = int(sys.argv[-1])
 
   lightserver.FluxHandler = fluxhandler.Lights(fake=fake, must_find_all=True)
   handler = lightserver.LightsHTTPRequestHandler
